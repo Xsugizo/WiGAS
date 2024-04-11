15af973123e5986ee9da6cdc68c638235730f3a7
WHQJonathan <152156312+WHQJonathan@users.noreply.github.com>
2024-04-11 16:16:18 +0800

    Update finalsendmail.py
    add email tiltle with retry reboot and notexecuteset num

--- IMAGE/workspace/Pipeline_Testing_Cts/finalsendmail.py
+++ IMAGE/workspace/Pipeline_Testing_Cts/finalsendmail.py
@@ -4,12 +4,30 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
+import subprocess
 import os
 import chardet
 
 import globalvar
 globalvar.initialize()
 dirpath = globalvar.path
+
+
+# email title add retry_num  retry_num reboot_num notexecuteset_num
+UsrName = subprocess.check_output('whoami')
+UsrName = UsrName.decode().strip()
+
+runfre = []
+with open(f'/home/{UsrName}/Desktop/IMAGE/workspace/runfre.txt') as file:
+    for line in file.readlines():
+        runfre.append(int(line))
+file.close()
+retry_num=runfre[0]
+reboot_num=runfre[1]
+reset_num=runfre[2]
+notexecuteset_num=runfre[3]
+subjectcontent='WiGAS Final Report with retry_num='+retry_num+' reboot_num='+reboot_num+' notexecuteset_num='+ notexecuteset_num
+
 
 # 設置SMTP伺服器地址和端口號
 smtp_server = 'smtp.office365.com'
@@ -43,7 +61,7 @@ msg.attach(html_part)
 
 # 登錄SMTP服務器
 smtp_username = 'Ian_H_Chang@wistron.com'
-smtp_password = 'Bk2Xt6jb222!'
+smtp_password = 'Bk2Xt6jb333!'
 smtp_obj = smtplib.SMTP(smtp_server, smtp_port)
 smtp_obj.starttls()
 smtp_obj.login(smtp_username, smtp_password)
